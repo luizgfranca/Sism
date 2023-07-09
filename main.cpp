ff68815fdc3b255be6660ba9373d6de81598e83e
luiz <luizgustavofcsg@hotmail.com>
2023-07-09 20:32:20 -0300

    tracer #0 - listing all units in a window

---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <sdbus-c++/IProxy.h>
+#include <sdbus-c++/Message.h>
+#include <sdbus-c++/Types.h>
+#include <sdbus-c++/sdbus-c++.h>
+#include <cstring>
+#include <string>
+#include <vector>
+#include "glibmm/refptr.h"
+#include "gtkmm/box.h"
+#include "gtkmm/liststore.h"
+#include "gtkmm/scrolledwindow.h"
+#include "gtkmm/treemodel.h"
+#include "gtkmm/treeview.h"
+#include <gtkmm-4.0/gtkmm.h>
+
+
+typedef     
+    std::vector<
+        sdbus::Struct<
+            std::string,
+            std::string,
+            std::string,
+            std::string,
+            std::string,
+            std::string,
+            sdbus::ObjectPath,
+            uint32_t,
+            std::string,
+            sdbus::ObjectPath
+        >
+    > ListUnitsResponse;
+
+void onConcatenated(sdbus::Signal& signal)
+{
+    std::string concatenatedString;
+    signal >> concatenatedString;
+
+    std::cout << "Received signal with concatenated string " << concatenatedString << std::endl;
+}
+
+ListUnitsResponse get_unit_list() {
+    const char* destinationName = "org.freedesktop.systemd1";
+    const char* objectPath = "/org/freedesktop/systemd1";
+    const char* interfaceName = "org.freedesktop.systemd1.Manager";
+
+    auto proxy = sdbus::createProxy(destinationName, objectPath);
+
+    proxy->finishRegistration();
+
+    ListUnitsResponse result;
+    proxy->callMethod("ListUnits").onInterface(interfaceName).storeResultsTo(result);
+
+    return result;
+}
+
+
+class MainWindow : public Gtk::Window {
+private:
+    class ColumnModel : public Gtk::TreeModel::ColumnRecord {
+    public:
+        ColumnModel() {
+            add(m_service_name);
+            add(m_service_status);
+            add(m_service_description);
+        }
+
+        Gtk::TreeModelColumn<Glib::ustring> m_service_name;
+        Gtk::TreeModelColumn<Glib::ustring> m_service_status;
+        Gtk::TreeModelColumn<Glib::ustring> m_service_description;
+    };
+
+    Gtk::TreeView m_treeview;
+    ColumnModel m_model;
+    Glib::RefPtr<Gtk::ListStore> m_tree_store;
+
+    void add_grid_item(std::string name, std::string status, std::string description) {
+        Gtk::TreeModel::Row row = *(m_tree_store->append());
+        row[m_model.m_service_name] = name;
+        row[m_model.m_service_status] = status;
+        row[m_model.m_service_description] = description;
+    }
+
+public:
+    MainWindow() {
+        set_title("Sism");
+        set_default_size(800, 800);
+        set_child(m_treeview);
+
+        m_tree_store = Gtk::ListStore::create(m_model);
+        m_treeview.set_model(m_tree_store);
+        m_treeview.append_column("Name", m_model.m_service_name);
+        m_treeview.append_column("Status", m_model.m_service_status);
+        m_treeview.append_column("Description", m_model.m_service_description);
+
+        auto units = get_unit_list();
+        for(auto unit : units) {
+            add_grid_item(unit.get<0>(), unit.get<2>(), unit.get<1>());
+        }
+    }
+};
+
+
+int main(int argc, char **argv) {
+    auto app = Gtk::Application::create("io.github.luizgfranca.sism");
+    return app->make_window_and_run<MainWindow>(argc, argv);
+
+    return 0;
+}
