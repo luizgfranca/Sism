efc555a0b77b741b6bdb1475a987a50677a31121
luizgfranca <luizgfc@proton.me>
2023-09-10 20:13:59 -0300

    Release scripts adjustments for RPM package build

--- tools/release.sh
+++ tools/release.sh
@@ -8,6 +8,7 @@ fi
 cd build
 cmake ..
 make
+make package
 cd ..
 
 RELEASE_PATH="dist/sism-$1"
@@ -22,6 +23,11 @@ cp build/sism "$RELEASE_PATH"
 cp tools/dependencies-fedora.sh "$RELEASE_PATH/scripts"
 cp tools/dependencies-ubuntu.sh "$RELEASE_PATH/scripts"
 cp tools/install.sh "$RELEASE_PATH"
+cp "build/sism-$1-1.x86_64.rpm" "dist/"
 
 cd dist
 zip -r "sism-$1.zip" "sism-$1" 
+
+echo "Release packaged successfully!"
+echo "Executable: $RELEASE_PATH.zip"
+echo "RPM Package: dist/sism-$1-1.x86_64.rpm"
